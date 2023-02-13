b12a0fa5325a3b7840cf708346f546c927cef2ac
NBhunter <bangnguyen01072001@gmail.com>
2023-02-13 21:20:09 +0700

    thêm chức năng thêm câu hỏi dành cho admin

--- resources/views/admin/P1cauhoi.blade.php
+++ resources/views/admin/P1cauhoi.blade.php
@@ -19,29 +19,34 @@
                         <form role="form" action="{{URL::to('/save-cauhoi')}}" method="post" enctype="multipart/form-data">
                             {{ csrf_field() }}
                             <div class="form-group">
+                                <label for="exampleInputPassword1">Cấp</label>
+                                  <select name="Cap" class="form-control input-sm m-bot15" onchange="changerID(this)">
+                                     <option value="0">tag (0)</option>
+                                        <option value="1" selected>Trụ Cột (1)</option>
+                                        <option value="2">Tiêu chí (2)</option>
+                                        <option value="3">câu hỏi (3)</option>
+                                </select>
+                            </div>
+                            <div class="form-group" id="tieuchi">
                                 <label for="exampleInputEmail1">Tiêu chí</label>
-                                <input type="text" data-validation="length" data-validation-length="min10" data-validation-error-msg="Làm ơn điền ít nhất 10 ký tự" name="chitiet_noidung" class="form-control " id="slug" placeholder="Trụ cột/ Thành phần/ Tiêu chí">
+                                <input type="text" data-validation="length" data-validation-length="min10" data-validation-error-msg="Làm ơn điền ít nhất 10 ký tự" name="NoiDung" class="form-control " id="NoiDung" placeholder="Trụ cột/ Thành phần/ Tiêu chí">
                             </div>
-                            <div class="form-group">
+                            <div class="form-group" id="cauhoi" hidden>
                                 <label for="exampleInputPassword1">Câu hỏi</label>
-                                <textarea style="resize: none"  rows="8" class="form-control" name="product_desc" id="ckeditor1" placeholder="Câu hỏi"></textarea>
+                                <textarea style="resize: none"  rows="8" class="form-control" name="TenCauHoi" id="ckeditor1" placeholder="Câu hỏi"></textarea>
                             </div>
-                            <div class="form-group">
+                            <div class="form-group"id="mota" hidden>
                                 <label for="exampleInputPassword1">Mô tả </label>
-                                <textarea style="resize: none"  rows="8" class="form-control" name="product_desc" id="ckeditor1" placeholder="Mô tả về câu hỏi"></textarea>
+                                <textarea style="resize: none"  rows="8" class="form-control" name="MoTa" id="ckeditor1" placeholder="Mô tả về câu hỏi"></textarea>
                             </div>
-                        <div class="form-group">
-                            <label for="exampleInputPassword1">Cấp</label>
-                              <select name="product_status" class="form-control input-sm m-bot15" onchange="changerID(this)">
-                                 <option value="0">tag (0)</option>
-                                    <option value="1" selected>Trụ Cột (1)</option>
-                                    <option value="2">Tiêu chí (2)</option>
-                                    <option value="3">câu hỏi (3)</option>
-                            </select>
+
+                        <div class="form-group" id="diem" >
+                            <label for="exampleInputEmail1">Điểm tối đa</label>
+                            <input type="text" data-validation="length" data-validation-length="min10" data-validation-error-msg="Làm ơn điền số điểm tối đa" name="DiemToiDa" class="form-control " id="DiemToiDa" placeholder="Trụ cột/ Thành phần/ Tiêu chí">
                         </div>
                         <div class="form-group" id="trucot"  hidden>
                             <label for="exampleInputPassword1">Trụ cột</label>
-                              <select name="product_status" class="form-control input-sm m-bot15">
+                              <select name="idcha1" class="form-control input-sm m-bot15">
                                 @php($i=0)
                         @foreach ( $Cauhoi as $key => $c )
                         @if ($c->Cap == 1 )
@@ -54,7 +59,7 @@
                         </div>
                         <div class="form-group" id="thanhphan" hidden>
                             <label for="exampleInputPassword1">Thành phần</label>
-                              <select name="product_status" class="form-control input-sm m-bot15">
+                              <select name="idcha2" class="form-control input-sm m-bot15">
                                 @php($j=0)
                                 @php($i=0)
                                 @foreach ( $Cauhoi as $key => $c )
@@ -87,16 +92,32 @@
     <script>
 function changerID(obj){
     var value = obj.value;
-    if(value =='2'||value =='0')
+    if(value =='2')
     {
+    document.getElementById("cauhoi").hidden = true;
+    document.getElementById("mota").hidden = true;
+    document.getElementById("diem").hidden = false;
     document.getElementById("thanhphan").hidden = true;
     document.getElementById("trucot").hidden = false;
     }else if(value =='3')
     {
+    document.getElementById("cauhoi").hidden = false;
+    document.getElementById("mota").hidden = false;
+    document.getElementById("diem").hidden = true;
     document.getElementById("trucot").hidden = true;
     document.getElementById("thanhphan").hidden = false;
+    }else if(value =='0')
+    {
+    document.getElementById("cauhoi").hidden = true;
+    document.getElementById("mota").hidden = true;
+    document.getElementById("diem").hidden = true;
+    document.getElementById("trucot").hidden = false;
+    document.getElementById("thanhphan").hidden = true;
     }
     else{
+    document.getElementById("cauhoi").hidden = true;
+    document.getElementById("mota").hidden = true;
+    document.getElementById("diem").hidden = false;
     document.getElementById("trucot").hidden = true;
     document.getElementById("thanhphan").hidden = true;
     }
