8259b3351a5fad6042e7725aa84b7deea273eb51
NBhunter <bangnguyen01072001@gmail.com>
2023-02-22 16:54:00 +0700

    Thêm dữ liệu  cho giao diện tin tức

--- resources/views/home/post.blade.php
+++ resources/views/home/post.blade.php
@@ -35,18 +35,18 @@
                         <div class="post-content">
 
                             <div class="text-center mb-50">
-                                <p class="post-date">MAY 01, 2018 / lifestyle</p>
-                                <h2 class="post-title">Express recipes: Give a twist to your classic moong dal with this recipe</h2>
+                                <p class="post-date">{{ $TinTuc->created_at }}</p>
+                                <h2 class="post-title">{{ $TinTuc->TieuDe }}</h2>
                                 <!-- Post Meta -->
                                 <div class="post-meta">
-                                    <a href="#"><span>by</span> Colorlib</a>
-                                    <a href="#">03 <span>Comments</span></a>
+                                    <a href="#"><span>by</span> demo</a>
+                                    {{-- <a href="#">03 <span>Comments</span></a> --}}
                                 </div>
                             </div>
 
                             <!-- Post Thumbnail -->
                             <div class="post-thumbnail mb-50">
-                                <img src="img/blog-img/8.jpg" alt="">
+                                <img src="{{ asset('img/AnhTinhTuc/'.$TinTuc->HinhAnh) }}" alt="">
                             </div>
 
                             <!-- Post Text -->
@@ -61,22 +61,7 @@
                                     <a href="#" class="pin"><i class="fa fa-thumb-tack" aria-hidden="true"></i></a>
                                 </div>
 
-                                <p>I love dals. All kinds of them but yellow moong dal is my go-to lentil when I am in need of some easy comfort food. In this recipe I added suva or dill leaves to the classic moong dal recipe for a twist. I like the simplicity of this recipe, just the dal, tomatoes and fresh dill with simple seasoning. This recipe is without any onions and garlic. I love the aroma of fresh dill and I think, in Indian food, we don’t really use dill as much as we can. Nine out of ten times, the only green leaves sprinkled on a curry or a dal is fresh coriander and while I love coriander too, dill adds a unique freshness and aroma to the dal. The delicate feathery leaves of dill are also rich in Vitamin A, C and minerals like iron and manganese.</p>
-
-                                <p>Dals or lentils are packed with proteins and especially in a vegetarian diet, lentils are the main source of protein. It is amazing how this humble yellow moong dal can be made into so many recipes from a plain dal khichdi to mangodi ki sabzi to the traditional Indian desserts like moong dal halwa. Fresh dill should be added only at the end of cooking, much like fresh coriander leaves. They don’t really need to cook and cooking for a long time actually reduces their flavour and aroma.</p>
-
-                                <blockquote class="shortcodes">
-                                    <div class="blockquote-icon">
-                                        <img src="img/core-img/quote.png" alt="">
-                                    </div>
-                                    <div class="blockquote-text">
-                                        <h5>That’s not to say you’ll have the exact same thing if you stop by: the restaurant’s menus change constantly, based on seasonal ingredients.</h5>
-                                        <h6>Ollie Schneider - <span>CEO Colorlib</span></h6>
-                                    </div>
-                                </blockquote>
-
-                                <p>Dals or lentils are packed with proteins and especially in a vegetarian diet, lentils are the main source of protein. It is amazing how this humble yellow moong dal can be made into so many recipes from a plain dal khichdi to mangodi ki sabzi to the traditional Indian desserts like moong dal halwa. Fresh dill should be added only at the end of cooking, much like fresh coriander leaves. They don’t really need to cook and cooking for a long time actually reduces their flavour and aroma.</p>
-
+                                <p>{!!$TinTuc->NoiDung!!}</p>
                                 <div class="row">
                                     <div class="col-12 col-md-6">
                                         <img class="mb-30" src="img/blog-img/4.jpg" alt="">
@@ -86,18 +71,7 @@
                                     </div>
                                 </div>
 
-                                <!-- List -->
-                                <div class="nikki-list-area mb-50">
-                                    <h4 class="mb-15">Immediate Dividends</h4>
-                                    <ul class="nikki-list">
-                                        <li>* Wash the dal in 3-4 changes of water and soak in room temperature water for 10 mins while you finish the rest of preparation.</li>
-                                        <li>* Drain and pressure cook with salt, turmeric and water for 2 whistles.</li>
-                                        <li>* Remove the cooker from heat and open only after all the steam has escaped on its own.</li>
-                                        <li>* While the dal is cooking, heat ghee in a pan. Add hing and cumin seeds.</li>
-                                        <li>* When the seeds start to crackle, add ginger, and green chillies. Sauté for a minute.</li>
-                                        <li>* Add tomatoes and a little salt. Mix well. Cook for about 5 mins with occasional stirring. Add a little water to the pan if the masala starts to stick.</li>
-                                    </ul>
-                                </div>
+
 
                                 <!-- Post Tags & Share -->
                                 <div class="post-tags-share">
