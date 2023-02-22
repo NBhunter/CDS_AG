097661aadc4777eee5805ee3e53a0d97a5a121bf
NBhunter <bangnguyen01072001@gmail.com>
2023-02-22 16:26:52 +0700

    thêm giao diện trang tin, thêm tin mới

---
+++ app/Models/tintuc.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class tintuc extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'Id',
+        'TieuDe',
+        'TomTat',
+        'NoiDung',
+        'HinhAnh',
+        'LuotXem',
+        'LinhVuc_id',
+        'Website',
+        'LoaiTin_id',
+        'Status',
+        ];
+}
