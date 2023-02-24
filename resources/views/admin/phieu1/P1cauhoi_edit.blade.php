2e5381d4b14db6d819ac1fcccf91632f0d509168
NBhunter <bangnguyen01072001@gmail.com>
2023-02-24 11:51:21 +0700

    thêm xử lý p1

---
+++ resources/views/admin/phieu1/P1cauhoi_edit.blade.php
@@ -0,0 +1,160 @@
+@extends('admin.admindashboard')
+@section('content')
+<div class="row">
+    <div class="col-lg-12">
+            <section class="panel">
+                <header class="panel-heading" style="font-weight:bold;text-align:center;font-size:26px;color:#4e73df;font-style:Roboto;">
+                   THÊM CÂU HỎI
+                </header>
+                 <?php
+                    $message = Session::get('message');
+                    if($message){
+                        echo '<span class="text-alert">'.$message.'</span>';
+                        Session::put('message',null);
+                    }
+                    ?>
+                <div class="panel-body">
+
+                    <div class="position-center">
+                        <form role="form" action="{{URL::to('/save-cauhoi')}}" method="post" enctype="multipart/form-data">
+                            {{ csrf_field() }}
+                            <div class="form-group">
+                                <label for="exampleInputPassword1">Cấp</label>
+                                  <select name="Cap" class="form-control input-sm m-bot15" onchange="changerID(this)">
+                                     <option value="0">Tiêu đề (0)</option>
+                                        <option value="1" selected>Trụ cột (1)</option>
+                                        <option value="2">Tiêu chí (2)</option>
+                                        <option value="3">Câu hỏi (3)</option>
+                                </select>
+                            </div>
+                            <div class="form-group" id="tieuchi">
+                                <label for="exampleInputEmail1">Tiêu chí</label>
+                                <input type="text" data-validation="length" data-validation-length="min10" data-validation-error-msg="Điền ít nhất 10 ký tự" name="NoiDung" class="form-control " id="NoiDung" placeholder="Nhập tiêu chí">
+                            </div>
+                            <div class="form-group" id="cauhoi" hidden>
+                                <label for="exampleInputPassword1">Câu hỏi</label>
+                                <textarea style="resize: none"  rows="8" class="form-control" name="TenCauHoi" id="ckeditor1" placeholder="Nhập câu hỏi"></textarea>
+                            </div>
+                            <div class="form-group"id="mota" hidden>
+                                <label for="exampleInputPassword1">Mô tả </label>
+                                <textarea style="resize: none"  rows="8" class="form-control" name="MoTa" id="ckeditor2" placeholder="Nhập mô tả về câu hỏi"></textarea>
+                            </div>
+
+                        <div class="form-group" id="diem" >
+                            <label for="exampleInputEmail1">Điểm tối đa</label>
+                            <input type="text" data-validation="length" data-validation-length="min10" data-validation-error-msg="Điền số điểm tối đa" name="DiemToiDa" class="form-control " id="DiemToiDa" placeholder=" Nhập điểm">
+                        </div>
+                        <div class="form-group" id="trucot"  hidden>
+                            <label for="exampleInputPassword1">Trụ cột</label>
+                              <select name="idcha1" class="form-control input-sm m-bot15">
+                                @php($i=0)
+                        @foreach ( $Cauhoi as $key => $c )
+                        @if ($c->Cap == 1 )
+                        @php($i++)
+                        <option value="{{ $c->idcauhoi }}"> {{ $c->NoiDung }} ({{ $i}})</option>
+                        @endif
+                        @endforeach
+
+                            </select>
+                        </div>
+                        <div class="form-group" id="thanhphan0"  hidden>
+                            <label for="exampleInputPassword1">Thành phần </label>
+                              <select name="idcha1" class="form-control input-sm m-bot15">
+                                @php($j=0)
+                                @php($i=0)
+                                @foreach ( $Cauhoi as $key => $c )
+                                @if ($c->Cap == 1)
+                                @php($i++)
+                                @php($j=0)
+                                @endif
+                                @if ($c->Cap == 2 )
+
+                                <option value="{{ $c->idcauhoi }}">@foreach ($Cauhoi as $key => $q )
+                                    @if ($q->idcauhoi ==$c->IdCha &&$q->Cap == 1)
+
+                                    {{ $q->NoiDung }}({{ $i}})|
+                                    @elseif ($q->idcauhoi ==$c->IdCha &&$c->Cap == 2)
+                                    {{ $q->NoiDung }}({{ $j}})|
+                                    @endif
+                                @endforeach {{ $c->NoiDung }} @if($c->Cap == 2)@php($j++)({{ $j}})@endif</option>
+                                @endif
+                                @endforeach
+
+                            </select>
+                        </div>
+                        <div class="form-group" id="thanhphan" hidden>
+                            <label for="exampleInputPassword1">Thành phần</label>
+                              <select name="idcha2" class="form-control input-sm m-bot15">
+                                @php($j=0)
+                                @php($i=0)
+                                @foreach ( $Cauhoi as $key => $c )
+                                @if ($c->Cap == 1)
+                                @php($i++)
+                                @endif
+                                @if ($c->Cap == 2 ||$c->Cap == 0)
+
+                                <option value="{{ $c->idcauhoi }}">@foreach ($Cauhoi as $key => $q )
+                                    @if ($q->idcauhoi ==$c->IdCha &&$q->Cap == 1)
+
+                                    {{ $q->NoiDung }}({{ $i}})|
+                                    @elseif ($q->idcauhoi ==$c->IdCha &&$q->Cap == 2)
+                                    {{ $q->NoiDung }}({{ $j}})|
+                                    @endif
+                                @endforeach {{ $c->NoiDung }} @if($c->Cap == 2)@php($j++)({{ $j}})@elseif ($c->Cap == 0)(0)@endif</option>
+                                @endif
+                                @endforeach
+
+                            </select>
+                        </div>
+                        <button type="submit" name="add_product" class="btn btn-info"><i class='fas fa-plus'></i>  Thêm câu hỏi</button>
+                        </form>
+                    </div>
+
+                </div>
+            </section>
+                </div>
+    </div>
+    <script>
+function changerID(obj){
+    var value = obj.value;
+    if(value =='2')
+    {
+    document.getElementById("cauhoi").hidden = true;
+    document.getElementById("mota").hidden = true;
+    document.getElementById("diem").hidden = false;
+    document.getElementById("thanhphan").hidden = true;
+    document.getElementById("trucot").hidden = false;
+    document.getElementById("thanhphan0").hidden = true;
+    }else if(value =='3')
+    {
+    document.getElementById("cauhoi").hidden = false;
+    document.getElementById("mota").hidden = false;
+    document.getElementById("diem").hidden = true;
+    document.getElementById("trucot").hidden = true;
+    document.getElementById("thanhphan").hidden = false;
+    document.getElementById("thanhphan0").hidden = true;
+    }else if(value =='0')
+    {
+    document.getElementById("cauhoi").hidden = true;
+    document.getElementById("mota").hidden = true;
+    document.getElementById("diem").hidden = true;
+    document.getElementById("thanhphan0").hidden = false;
+    document.getElementById("trucot").hidden = true;
+    document.getElementById("thanhphan").hidden = true;
+    }
+    else{
+    document.getElementById("cauhoi").hidden = true;
+    document.getElementById("mota").hidden = true;
+    document.getElementById("diem").hidden = false;
+    document.getElementById("trucot").hidden = true;
+    document.getElementById("thanhphan").hidden = true;
+    document.getElementById("thanhphan0").hidden = true;
+    }
+}
+    </script>
+     <script src="//cdn.ckeditor.com/4.16.2/full-all/ckeditor.js"></script>
+     <script>
+     CKEDITOR.replace( 'ckeditor1' );
+     CKEDITOR.replace( 'ckeditor2' );
+     </script>
+@endsection
---
+++ resources/views/admin/phieu1/cauhoi_1_list.blade.php
@@ -0,0 +1,153 @@
+@extends('admin.admindashboard')
+@section('content')
+{{--
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script> --}}
+
+
+    <!-- Main Content -->
+
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+            <!-- Page Heading -->
+            <h1 style="font-weight:bold;text-align:center;font-size:26px;color:#4e73df;font-style:Roboto;">THÔNG TIN NGƯỜI DÙNG</h1>
+
+            <!-- DataTales Example -->
+            <div class="card mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">Danh sách người dùng</h6>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-bordered" id="dataTable"style="text-align:center;">
+                            <thead>
+                                <tr>
+                                    <td class="col-1">STT</td>
+                                    <td class="col-3">Trụ cột/ Thành phần/ Tiêu chí</td>
+                                    <td class="col-5">Câu hỏi/Kê khai số liệu</td>
+                                    <td class="col-3">Thang điểm tối đa</td>
+                                    <td class="col-2">Sửa</td>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                @php
+    $i=0;
+    $j=0;
+    $y=0;
+@endphp
+                                @foreach($Cauhoi as $key => $ctCauhoi)
+                                @if ($ctCauhoi->Cap==1)
+                                @php
+                                    $i++;
+                                    $j=0;
+                                @endphp
+                                <tr class=""style="text-align:center;font-weight:400;">
+                                    <th class="stt" >{{ $i }}</th>
+                                    <th class="">{{ $ctCauhoi->NoiDung }}</th>
+                                    <th class=""> </th>
+                                    <th class=""  style="text-align:center;">{{ $ctCauhoi->DiemToiDa }}</th>
+                                    <th class=""><a class="btn btn-success" href="{{ URL::to('/admin/edit/'.$ctCauhoi->idcauhoi) }}"><i class='fas fa-edit'></i></a> </th> </th>
+                                </tr>
+                                @endif
+                                @if ($ctCauhoi->Cap==2&& $i>0)
+                                @php
+                                    $j++;
+                                    $y=0;
+                                @endphp
+                                <tr class="" style="text-align:center;font-weight:400;">
+                                    <th class="stt" style="text-align:center;">{{ $i.".".$j }}</th>
+                                    <th class="">{{ $ctCauhoi->NoiDung }}</th>
+                                    <th class=""> </th>
+                                    <th class=""  style="text-align:center;">{{ $ctCauhoi->DiemToiDa }}</th>
+                                    <th class=""><a class="btn btn-success" href="{{ URL::to('/admin/edit/'.$ctCauhoi->idcauhoi) }}"><i class='fas fa-edit'></i></a> </th> </th>
+                                </tr>
+                                @endif
+                                @if ($ctCauhoi->Cap==0&& $i> 0)
+
+                                <tr class="" style="text-align:center;font-weight:400;">
+                                    <th class="stt" > </th>
+                                    <th class="" style="text-align:center;">{{ $ctCauhoi->NoiDung }}</th>
+                                    <th class=""> </th>
+                                    <th class=""> </th>
+                                    <th class=""><a class="btn btn-success" href="{{ URL::to('/admin/edit/'.$ctCauhoi->idcauhoi) }}"><i class='fas fa-edit'></i></a> </th> </th></tr>
+                                @endif
+                                @if ($ctCauhoi->Cap==3&& $i>0 &&$j >0)
+                                @php
+                                    $y++;
+                                @endphp
+                                <tr class="" style="text-align:center;font-weight:400;">
+                                    <th rowspan="2" style="text-align:center;">{{ $i.".".$j.".".$y}}</th>
+                                    <th rowspan="2" >{{ $ctCauhoi->NoiDung }}</th>
+                                    <th rowspan="1"class="noidung"> {{ $ctCauhoi->TenCauHoi }}   </th>
+
+                                    <th rowspan="2"> </th>
+                                    {{-- <th rowspan="2"> </th> --}}
+                                    <th rowspan="2">
+                                        <a class="btn btn-success" href="{{ URL::to('/admin/edit/'.$ctCauhoi->idcauhoi) }}"><i class='fas fa-edit'></i></a> </th>
+                                </tr>
+                                <tr class="" > <th rowspan="1" class="noidung">{{ $ctCauhoi->MoTa }}</th></tr>
+
+                                @endif
+
+
+                                  @endforeach
+
+
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+        <!-- /.container-fluid -->
+
+    </div>
+    <!-- End of Main Content -->
+
+
+
+</div>
+<!-- End of Content Wrapper -->
+
+</div>
+<!-- End of Page Wrapper -->
+
+<!-- Scroll to Top Button-->
+<a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+
+
+{{-- <script src="{{asset('admin/vendor/jquery/jquery.min.js')}}"></script> --}}
+    <script src="{{asset('admin/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="{{asset('admin/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="{{asset('admin/js/sb-admin-2.min.js')}}"></script>
+<!-- Bootstrap core JavaScript-->
+<script>
+function change($a) {
+    $.post(
+    {
+      name: "Donald Duck",
+      city: "Duckburg"
+    },
+    function(data,status){
+      alert("Data: " + data + "\nStatus: " + status);
+    });
+}
+$("admin").click(function(){
+    alert("Data: ");
+  });
+</script>
+
+@endsection
