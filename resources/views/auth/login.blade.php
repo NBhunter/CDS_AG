87dadc135a60fbc059a8717ce4431840ea9426be
NgocMinh66 <nn4966@gmail.com>
2023-04-05 16:42:05 +0700

    chinh login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -22,7 +22,7 @@
 <x-guest-layout>
 <section class="vh-100">
     <video autoplay muted loop id="myVideo">
-        <source src="{{asset('logout/img/bg.mp4')}}" type="video/mp4">
+        <source src="{{asset('logout_file/img/bg.mp4')}}" type="video/mp4">
       </video>
     <div class="container-fluid h-custom">
 
@@ -30,8 +30,8 @@
 
           <form method="POST" action="{{ route('login') }}">
             @csrf
-            <div class="d-flex flex-row align-items-center justify-content-center justify-content-lg-start" ALIGN:CENTER;>
-              <p class="lead fw-normal mb-0 me-3">ĐĂNG NHẬP VỚI</p>
+            <div class="d-flex flex-row align-items-center justify-content-center justify-content-lg-start" ALIGN="CENTER";>
+              <p class="lead fw-normal mb-0 me-3" style="font-weight:bold;">ĐĂNG NHẬP VỚI</p>
               <button type="button" class="btn btn-primary btn-floating mx-1">
                 <i class="fab fa-facebook-f"></i>
               </button>
@@ -40,7 +40,7 @@
                 <i class="fab fa-twitter"></i>
               </button>
 
-              <button type="button" class="btnbtn-floating mx-1">
+              <button type="button" class="btn btn-floating mx-1">
                 <i class="fab fa-linkedin-in"></i>
               </button>
             </div>
@@ -54,18 +54,21 @@
                 <x-jet-label for="email" value="{{ __('Email') }}" />
                 <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
             </div> --}}
-            <div class="form-outline mb-4">
+            <div class="form-outline mb-4" style="font-family: Roboto;">
+                <x-jet-label for="auth" class="form-label" value="{{ __('Email / Số điện thoại') }}" />
                 <x-jet-input id="auth" class="form-control form-control-lg block mt-1 w-full" type="text" name="auth" :value="old('auth')" required autofocus />
-                <x-jet-label for="auth" class="form-label" value="{{ __('Email/Số điện thoại') }}" />
+
                 {{-- <input type="email" id="email" class="form-control form-control-lg"
                 placeholder="Enter a valid email address" type="email" name="email" :value="old('email')" required autofocus  />
               <label class="form-label" for="form3Example3">Email address</label> --}}
             </div>
 
             <!-- Password input -->
-            <div class="form-outline mb-3">
-                <x-jet-input id="password" class="block mt-1 w-full form-control form-control-lg" type="password" name="password" required autocomplete="current-password" />
+            <br>
+            <div class="form-outline mb-3" style="font-family: Roboto;">
                 <x-jet-label for="password" class="form-label" value="{{ __('Mật khẩu') }}" />
+                <x-jet-input id="password" class="block mt-1 w-full form-control form-control-lg" type="password" name="password" required autocomplete="current-password" />
+
                 {{-- <input id="password"  type="password" name="password" required autocomplete="current-password"  class="form-control form-control-lg"
                 placeholder="Enter password" />
               <label class="form-label" for="form3Example4">Password</label> --}}
@@ -73,9 +76,9 @@
 
             <div class="d-flex justify-content-between align-items-center">
               <!-- Checkbox -->
-              <div class="form-check mb-0">
+              <div class="form-check mb-0" style="font-family: Roboto;">
                 <x-jet-checkbox id="remember_me" name="remember" class="form-check-input me-2"/>
-                    <span class=" form-check-label ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    <span class=" form-check-label ml-2 text-sm text-gray-600">{{ __('Ghi nhớ mật khẩu') }}</span>
                 {{-- <input class="form-check-input me-2" type="checkbox" value="" id="remember_me" name="remember" />
                 <label class="form-check-label" for="form2Example3">
                   Remember me
@@ -83,7 +86,7 @@
               </div>
               @if (Route::has('password.request'))
               <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                  {{ __('Forgot your password?') }}
+                  {{ __('Bạn đã quên mật khẩu ?') }}
               </a>
           @endif
             </div>
