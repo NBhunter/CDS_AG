916f1e316f750e908a05b5a48906b05568743392
NBhunter <bangnguyen01072001@gmail.com>
2023-04-12 11:11:02 +0700

    thêm chức năng bình luận, rsp Login form

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,14 +25,21 @@ class HomeController extends Controller
     public function index()
     {
         $slides = DB::table('slides')->get();
-        $tinmoi = DB::table('tintucs')->leftjoin('linhvuc', 'linhvuc.Id', '=', 'tintucs.LinhVuc_id')->select('tintucs.Id as IdTin', 'tintucs.*', 'linhvuc.*')->orderBy('updated_at', 'desc')->limit(5)->get();
+        $tinmoi = DB::table('tintucs')->leftjoin('linhvuc', 'linhvuc.Id', '=', 'tintucs.LinhVuc_id')->select('tintucs.Id as IdTin', 'tintucs.*', 'linhvuc.*')->orderBy('updated_at', 'desc')->paginate(6);
+
+        return view('home.home')->with('slides', $slides)->with('tinmoi', $tinmoi);
+    }
+    public function indexAllnew()
+    {
+        $slides = DB::table('slides')->get();
+        $tinmoi = DB::table('tintucs')->leftjoin('linhvuc', 'linhvuc.Id', '=', 'tintucs.LinhVuc_id')->select('tintucs.Id as IdTin', 'tintucs.*', 'linhvuc.*')->orderBy('updated_at', 'desc')->paginate(6);
 
         return view('home.home')->with('slides', $slides)->with('tinmoi', $tinmoi);
     }
     public function NongNghiepIndex()
     {
         $slides = DB::table('slides')->get();
-        $tinmoi = DB::table('tintucs')->leftjoin('linhvuc', 'linhvuc.Id', '=', 'tintucs.LinhVuc_id')->select('tintucs.Id as IdTin', 'tintucs.*', 'linhvuc.*')->orderBy('updated_at', 'desc')->limit(5)->get();
+        $tinmoi = DB::table('tintucs')->leftjoin('linhvuc', 'linhvuc.Id', '=', 'tintucs.LinhVuc_id')->select('tintucs.Id as IdTin', 'tintucs.*', 'linhvuc.*')->orderBy('updated_at', 'desc')->paginate(6);
 
         return view('home.TinLinhVuc')->with('tinmoi', $tinmoi)->with('title', "Tin tức nông nghiệp");
     }
@@ -47,5 +54,31 @@ class HomeController extends Controller
         $LoaiHinh = DB::table('nganhnghe')->get();
 
         return view('home.dangkydoanhnghiep')->with('LinhVuc',$LinhVuc)->with('LoaiHinh',$LoaiHinh);
+    }
+    public function postcomment(Request $request)
+    {
+        $input = $request->collect();
+        $cmt = array();
+
+        if(isset($input['reply_id']))
+        {
+            $cmt['IdCon'] = $input['reply_id'];
+        }
+        $cmt['NoiDung']= $input['message'];
+        $cmt['TrangThai'] = '0';
+        $cmt['email'] = $input['Email'];
+        $cmt['TenNguoiBL'] = $input['Name'];
+        $cmt['TinTuc_id'] = $input['IdNews'];
+        $cmt['NgayCMT'] = date('Y-m-d');
+        if (DB::table('binhluan')->insert($cmt))
+        {
+            $alert = "đã thêm bình luận";
+        }
+        else{
+            $alert =" Bình luận không thành công";
+        }
+
+        return redirect()->back()->with('alert',$alert);
+
     }
 }
